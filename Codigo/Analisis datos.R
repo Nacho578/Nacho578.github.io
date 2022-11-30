fa93887f4d63b4bf5a8d024e20fbb4f09d4d1ea3
Ignacio Pavez Avendaño <ignacio.pavez@uc.cl>
2022-11-29 21:28:14 -0300

    Entrega reporte

--- Codigo/Analisis datos.R
+++ Codigo/Analisis datos.R
@@ -5,27 +5,41 @@ library(tidyr)
 library(stringr)
 
 
-datos_oro <- read_csv("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos Futuros oro.csv")
-datos_TSLA <- read_csv("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos TSLA (1).csv")
-datos_sp500 <- read_csv("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos S&P 500.csv")
-tail(datos_sp500)
-tail(datos_TSLA)
-tail(datos_oro)
+datos_EURJPY <- read_csv("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos EUR_JPY (1).csv")
+datos_GBPUSD <- read_csv("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos GBP_USD.csv")
 
 
 
-names(datos_oro)
 
-datos_oro$Fecha <- as.Date(datos_oro$Fecha, format = "%d.%m.%Y")
-datos_TSLA$Fecha <- as.Date(datos_TSLA$Fecha, format = "%d.%m.%Y")
-datos_sp500$Fecha <- as.Date(datos_sp500$Fecha, format = "%d.%m.%Y")
-
-datos_oro$Vol. <- NULL
-datos_sp500$Vol. <- NULL
-datos_TSLA$Vol. <- NULL
+datos_EURJPY$Fecha <- as.Date(datos_EURJPY$Fecha, format = "%d.%m.%Y")
+datos_GBPUSD$Fecha <- as.Date(datos_GBPUSD$Fecha, format = "%d.%m.%Y")
 
 
+datos_EURJPY$Vol. <- NULL
+datos_GBPUSD$Vol. <- NULL
 
-write_csv(datos_oro, "C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos Futuros oro.csv")
-write_csv(datos_sp500,"C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos S&P 500.csv")
-write_csv(datos_TSLA,"C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos TSLA (1).csv")
+
+datos_EURJPY$Último <- datos_EURJPY$Último/100
+datos_EURJPY$Apertura <- datos_EURJPY$Apertura/100
+datos_EURJPY$Máximo <- datos_EURJPY$Máximo/100
+datos_EURJPY$Mínimo <- datos_EURJPY$Mínimo/100
+
+datos_GBPUSD$Último <- datos_GBPUSD$Último/10000
+datos_GBPUSD$Apertura <- datos_GBPUSD$Apertura/10000
+datos_GBPUSD$Máximo <- datos_GBPUSD$Máximo/10000
+datos_GBPUSD$Mínimo <- datos_GBPUSD$Mínimo/10000
+
+datos_EURJPY$`% var.` <- str_sub(datos_EURJPY$`% var.`, 1, nchar(datos_EURJPY$`% var.`)-1 )
+datos_EURJPY$`% var.` <- str_replace(datos_EURJPY$`% var.`, ",", ".")
+datos_EURJPY$`% var.` <- as.numeric(datos_EURJPY$`% var.`)
+
+datos_GBPUSD$`% var.` <- str_sub(datos_GBPUSD$`% var.`, 1, nchar(datos_GBPUSD$`% var.`)-1 )
+datos_GBPUSD$`% var.` <- str_replace(datos_GBPUSD$`% var.`, ",", ".")
+datos_GBPUSD$`% var.` <- as.numeric(datos_GBPUSD$`% var.`)
+
+
+
+write.csv(datos_EURJPY, "C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos EUR_JPY (1).csv")
+write.csv(datos_GBPUSD, "C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos GBP_USD.csv")
+
+
--- Codigo/visualizacion.R
+++ Codigo/visualizacion.R
@@ -8,10 +8,139 @@ library(stringr)
 library(rebus) 
 library(gt)
 library(gtsummary)
+library(stats)
+library(zoo)
+library(pracma)
 
 
-datos_oro <- read_csv("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos Futuros oro.csv")
-datos_TSLA <- read_csv("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos TSLA (1).csv")
-datos_sp500 <- read_csv("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos S&P 500.csv")
+
+datos_EURJPY <- read_csv("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos EUR_JPY (1).csv")
+datos_GBPUSD <- read_csv("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Datos/Datos históricos GBP_USD.csv")
 
 
+var(datos_EURJPY$`% var.`)
+var(datos_GBPUSD$`% var.`)
+
+###Ganancias Maximas----
+
+max_winEURJPY <- ((datos_EURJPY$Máximo - datos_EURJPY$Mínimo)/datos_EURJPY$Máximo)*datos_EURJPY$...1
+datos_EURJPY$acumuladaEURJPY <- rev(cumsum(max_winEURJPY))
+
+max_winGBPUSD <- ((datos_GBPUSD$Máximo - datos_GBPUSD$Mínimo)/datos_GBPUSD$Máximo)*datos_EURJPY$...1
+datos_GBPUSD$acumuladaGBPUSD <- rev(cumsum(max_winGBPUSD))
+
+ggplot(datos_GBPUSD, aes(x = Fecha, acumuladaGBPUSD))+
+  geom_line(size = 1, color = '#1fb791') +
+  theme_minimal()+ 
+  labs(title = 'Ganancia Maxima GBPUSD',
+       y = 'Multiplicador de dinero')
+
+ggsave("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Figuras/MAXgbp.png", width = 10, height = 7)
+
+ggplot(datos_EURJPY, aes(x = Fecha, acumuladaEURJPY))+
+  geom_line(size = 1, color = '#1fb791') +
+  theme_minimal() + 
+  labs(title = 'Ganancia MaximaEURJPY',
+       y = 'Multiplicador de dinero')
+ggsave("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Figuras/MAXeur.png", width = 10, height = 7)
+
+
+###Estrategia Trading----
+sma <- function(x,n){
+  rollmean(x, n, fill = NA, align = "right")
+}
+
+EMA <- function(x, nn){
+  movavg(x, n = nn, type = "e")
+}
+
+myRSI <- function (price,n){
+  N <- length(price)
+  U <- rep(0,N)
+  D <- rep(0,N)
+  rsi <- rep(NA,N)
+  Lprice <- lag(price,1)
+  for (i in 2:N){
+    if (price[i]>=Lprice[i]){
+      U[i] <- 1
+    } else {
+      D[i] <- 1
+    }
+    if (i>n){
+      AvgUp <- mean(U[(i-n+1):i])
+      AvgDn <- mean(D[(i-n+1):i])
+      rsi[i] <- AvgUp/(AvgUp+AvgDn)*100 
+    }
+  }
+
+  return(rsi)
+}
+
+rsi <-myRSI(datos_EURJPY$Último, 14)
+
+length(datos_EURJPY)
+
+datos_EURJPY$EMA_50EURJPY <- EMA(datos_EURJPY$Apertura, 50)
+
+datos_EURJPY$EMA_200EURJPY <- EMA(datos_EURJPY$Apertura, 200)
+
+datos_GBPUSD$EMA_50GBPUSD <- EMA(datos_GBPUSD$Apertura, 50)
+
+datos_GBPUSD$EMA_200GBPUSD <- EMA(datos_GBPUSD$Apertura, 200)
+
+
+
+
+
+
+
+
+
+plot(datos_EURJPY$Fecha, datos_EURJPY$EMA_50EURJPY, type = 'l', xlab = 'Fecha', ylab = 'EMA', main = 'Trading Cruces de EMA EURJPY', col = 'BLue', lwd = 2)
+lines(datos_EURJPY$Fecha, datos_EURJPY$EMA_200EURJPY, col = 'green', lwd=3)
+legend("bottom", legend = c("EMA50", "EMA200"), col = c(2, 3))
+
+
+ggsave("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Figuras/EMAEURJPY.png", width = 10, height = 7)
+
+plot(datos_GBPUSD$Fecha, datos_GBPUSD$EMA_50GBPUSD, type = 'l', xlab = 'Fecha', ylab = 'EMA', main = 'Trading Cruces de EMA GBPUSD', col = 'BLue', lwd = 2)
+lines(datos_GBPUSD$Fecha, datos_GBPUSD$EMA_200GBPUSD, col = 'green', lwd=3)
+legend("bottom", legend = c("EMA50", "EMA200"), col = c(2, 3))
+
+
+ggsave("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Figuras/EMAGBPUSD.png", width = 10, height = 7)
+
+
+
+
+###Estrategia Value investing----
+
+promedioEURJPY <- cumsum(rev(datos_EURJPY$Apertura))/(datos_EURJPY$...1)
+ajuste_gananciasEURJPY <- ((rev(datos_EURJPY$Apertura)-promedioEURJPY)*datos_EURJPY$...1)
+
+datos_EURJPY$graficoEURJPY <- rev(ajuste_gananciasEURJPY/promedioEURJPY)
+
+
+
+
+promedioGBPUSD <- cumsum(rev(datos_GBPUSD$Apertura))/(datos_GBPUSD$...1)
+ajuste_gananciasGBPUSD <- ((rev(datos_GBPUSD$Apertura)-promedioGBPUSD)*datos_GBPUSD$...1)
+
+datos_GBPUSD$graficoGBPUSD <- rev(ajuste_gananciasGBPUSD/promedioGBPUSD)
+
+
+ggplot(datos_GBPUSD, aes(x = Fecha, graficoGBPUSD))+
+  geom_line(size = 1, color = '#63B8FF') +
+  theme_minimal()+ 
+  labs(title = 'Dollar cost Averaging GBPUSD',
+       y = 'Multiplicador de dinero')
+
+ggsave("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Figuras/dcagbp.png", width = 10, height = 7)
+
+ggplot(datos_EURJPY, aes(x = Fecha, graficoEURJPY))+
+  geom_line(size = 1, color = '#63B8FF') +
+  theme_minimal() + 
+  labs(title = 'Dollar cost Averaging EURJPY',
+       y = 'Multiplicador de dinero')
+
+ggsave("C:/Users/nachi/OneDrive/Desktop/Programacion/R/Let/Habilidades_Comunicativas/Figuras/dcaeur.png", width = 10, height = 7)
